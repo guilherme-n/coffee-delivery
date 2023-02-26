3811651c109da01d7a1135575e056ac6bc50372c
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-02-26 02:57:33 -0300

    add all theme colors

--- src/styles/themes/theme.ts
+++ src/styles/themes/theme.ts
@@ -1,3 +1,21 @@
 export interface Theme {
+	white: string;
+	background: string;
+
+	yellow: string;
+	'yellow-light': string;
 	'yellow-dark': string;
+
+	purple: string;
+	'purple-dark': string;
+	'purple-light': string;
+
+	'base-title': string;
+	'base-subtitle': string;
+	'base-text': string;
+	'base-label': string;
+	'base-hover': string;
+	'base-button': string;
+	'base-input': string;
+	'base-card': string;
 }
