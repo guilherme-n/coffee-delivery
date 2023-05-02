553477718c90df782776c9c03613a9dc47a33c56
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-05-02 12:24:26 -0300

    setup jest and @testing-library/react

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import babel from '@babel/plugin-syntax-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), babel()],
 	server: {
 		port: 3000,
 	},
